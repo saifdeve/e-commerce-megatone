9c3b3adbda3960ab1fbb10bdd423d96a8a2e864b
saifdeve <saiferrabeh@gmail.com>
2024-04-08 23:30:56 +0000

    modification-1

--- src/components/Product/product.js
+++ src/components/Product/product.js
@@ -4,8 +4,10 @@ import './product.css'
 import Rating from '@mui/material/Rating';
 import Button from '@mui/material/Button';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import RepeatIcon from '@mui/icons-material/Repeat';
+import VisibilityIcon from '@mui/icons-material/Visibility';
 
-export default function Produt(){
+export default function Produt({badge,image,titleCategory,titleProduct,ByWho}){
     const [rationValue,setRationValue]= useState(3.5);
     function handleRationChange(event, newValue){
         setRationValue(newValue); // Mettre à jour la valeur du rating
@@ -14,17 +16,24 @@ export default function Produt(){
     return(
         <div className='content-product'>
             <div className='top-card'>
-               <div className='product-badges'>Hot</div>
-               <img className='product-img' src='https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg' alt='img-1'/>
+               <div className='product-badges'>{badge}</div>
+               <img className='product-img' src={image} alt='img-1'/>
+               <div className='product-action'>
+                    <ul className='list-action'>
+                        <li className='action' tooltip="Add To Wishlist"><button><FavoriteBorderIcon className='icon'/></button></li>
+                        <li className='action' tooltip="Compare"><button><RepeatIcon className='icon'/></button></li>
+                        <li className='action' tooltip="Quick View"><button><VisibilityIcon className='icon'/></button></li>
+                    </ul>
+               </div>
             </div>
             <div className='botton-card'>
-                <h6 >Hodo Foods</h6>
-                <h4>All Natural Italian-Style Chicken Meatballs</h4>
+                <h6 >{titleCategory}</h6>
+                <h4>{titleProduct}</h4>
                 <div className='ration'>
                     <Rating name="half-rating" value={rationValue} precision={0.5} readOnly onChange={handleRationChange}/>
                     <span className='rationValue'>({rationValue % 2 !== 0 ? rationValue.toFixed(1):rationValue})</span>
                 </div>
-                <h6>By <span>NestFood</span></h6>
+                <h6>By <span>{ByWho}</span></h6>
                 <Button><FavoriteBorderIcon className='iconWishList'/><span>Add</span></Button>
             </div>
         </div>
--- src/pages/Home/prodHome/prodHome.js
+++ src/pages/Home/prodHome/prodHome.js
@@ -2,7 +2,25 @@ import './prodHome.css';
 import Product from "../../../components/Product/product"
 
 export default function ProdHome(){
-    const titleNav =[{id:1,title:"All"},{id:2,title:"Milks & Dairies"},{id:3,title:"Coffes & Teas"},{id:4,title:"Pet Foods"},{id:5,title:"Meats"},{id:6,title:"Vegetables"},{id:7,title:"Fruits"}]
+    const titleNav =[   {id:1,title:"All"},
+                        {id:2,title:"Milks & Dairies"},
+                        {id:3,title:"Coffes & Teas"},
+                        {id:4,title:"Pet Foods"},
+                        {id:5,title:"Meats"},
+                        {id:6,title:"Vegetables"},
+                        {id:7,title:"Fruits"}
+                    ];
+    const Products =[   {id:1,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:2,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:3,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:4,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:5,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:6,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:7,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:8,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:9,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"},
+                        {id:10,badge:"Hot",img:"https://wp.alithemes.com/html/nest/demo/assets/imgs/shop/product-1-1.jpg",titleCategory:"Hodo Foods",titleProduct:"All Natural Italian-Style Chicken Meatballs",ByWho:"NestFood"}
+                    ];
     return(
         <div className='container-products'>
             <div className='navProducts'>
@@ -16,16 +34,11 @@ export default function ProdHome(){
                 </ul>
             </div>
             <div className='list-product'>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
-                <Product/>
+                {
+                    Products.map((product)=>(
+                        <Product key={product.id} badge={product.badge} image={product.img} titleCategory={product.titleCategory} titleProduct={product.titleProduct} ByWho={product.ByWho}/>
+                    ))
+                }
             </div>
 
         </div>
